689f776537a36d0102e01643f10b13a4a9aa10af
Philip Baideme <pbaideme@primetherapeutics.com>
2021-08-11 19:53:59 +0000

    Added s3 bucket module_fix_6

--- main.tf
+++ main.tf
@@ -132,9 +132,7 @@ resource "aws_instance" "hashicat" {
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
   tags = {
-    Name = "${var.prefix}-hashicat-instance",
-    Name = "${var.prefix}-aws_instance",
-    "Department, Billable"
+    Name = "Department, Billable"
   }
 }
 
